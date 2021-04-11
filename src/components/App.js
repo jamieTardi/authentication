3683b8ae1acfe3308410c8349f6be966c3aaad84
Jamie Tardi <jamie.tardi@gmail.com>
2021-04-11 11:35:38 +0100

    created dashboard component, added react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
-import { SignUp } from './index';
+import { SignUp, DashBoard } from './index';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './../styles/style.css';
 import { AuthProvider } from '../context/AuthContext';
+import { Switch, Route } from 'react-router-dom';
 function App() {
 	return (
 		<AuthProvider>
-			<div className='App container  d-flex align-items-center justify-content-center'>
-				<div className='w-100 sign-up'>
-					<SignUp />
+			<Switch>
+				<div className='App container  d-flex align-items-center justify-content-center'>
+					<div className='w-100 sign-up'>
+						<Route exact path='/' component={DashBoard} />
+						<Route path='/signup' exact component={SignUp} />
+					</div>
 				</div>
-			</div>
+			</Switch>
 		</AuthProvider>
 	);
 }
