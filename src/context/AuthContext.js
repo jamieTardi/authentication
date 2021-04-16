b81959ba9f95689f687551611d45bb88ea0cb48a
Jamie Tardi <jamie.tardi@gmail.com>
2021-04-16 20:19:03 +0100

    adding credentials for sign up

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -11,6 +11,7 @@ export function AuthProvider({ children }) {
 	const [currentUser, setCurrentUser] = useState();
 	const [loading, setLoading] = useState(true);
 
+	//sign up credentials
 	function signup(email, password) {
 		return auth.createUserWithEmailAndPassword(email, password);
 	}
